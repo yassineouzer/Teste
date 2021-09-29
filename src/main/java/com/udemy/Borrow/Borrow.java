51478dd62fcbd36c42db3c43fba9e9e08c2d64c5
OUZEREN YASSINE <Yassineouzeren@outlook.fr>
2021-09-29 13:56:24 +0100

    finition des entities

--- src/main/java/com/udemy/Borrow/Borrow.java
+++ src/main/java/com/udemy/Borrow/Borrow.java
@@ -1,8 +1,100 @@
 package com.udemy.Borrow;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.udemy.Teste.Book.Book;
+import com.udemy.Teste.Book.User.User;
+
 
 @Entity
 public class Borrow {
     
+   
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)   
+    private int Id;
+    @ManyToOne
+    private User lender;
+    @ManyToOne
+    private User borrower;
+    @ManyToOne
+    private Book book;
+    private LocalDate askdate;
+
+    /**
+     * @return int return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
+    }
+
+    /**
+     * @return User return the lender
+     */
+    public User getLender() {
+        return lender;
+    }
+
+    /**
+     * @param lender the lender to set
+     */
+    public void setLender(User lender) {
+        this.lender = lender;
+    }
+
+    /**
+     * @return User return the borrower
+     */
+    public User getBorrower() {
+        return borrower;
+    }
+
+    /**
+     * @param borrower the borrower to set
+     */
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+    /**
+     * @return Book return the book
+     */
+    public Book getBook() {
+        return book;
+    }
+
+    /**
+     * @param book the book to set
+     */
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    /**
+     * @return LocalDate return the askdate
+     */
+    public LocalDate getAskdate() {
+        return askdate;
+    }
+
+    /**
+     * @param askdate the askdate to set
+     */
+    public void setAskdate(LocalDate askdate) {
+        this.askdate = askdate;
+    }
+
 }
--- src/main/java/com/udemy/Teste/Book/Category/Category.java
+++ src/main/java/com/udemy/Teste/Book/Category/Category.java
@@ -1,11 +1,24 @@
 package com.udemy.Teste.Book.Category;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
-
+@Entity
 public class Category {
 
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
     private String label;
 
+
+    public Category() {
+        
+	}
 
 
     public Category(String label) {
@@ -25,6 +38,21 @@ public class Category {
      */
     public void setLabel(String label) {
         this.label = label;
+    }
+
+
+    /**
+     * @return int return the Id
+     */
+    public int getId() {
+        return Id;
+    }
+
+    /**
+     * @param Id the Id to set
+     */
+    public void setId(int Id) {
+        this.Id = Id;
     }
 
 }
