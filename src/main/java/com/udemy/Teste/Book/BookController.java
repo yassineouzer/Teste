34723ded512843f3eb35a4c58968c8641c3d497c
OUZEREN YASSINE <Yassineouzeren@outlook.fr>
2021-10-02 13:31:50 +0100

    commit des modifications

--- src/main/java/com/udemy/Teste/Book/BookController.java
+++ src/main/java/com/udemy/Teste/Book/BookController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import com.udemy.Teste.Book.Category.Category;
+import com.udemy.Teste.Book.Category.CategoryRepository;
 import com.udemy.Teste.Book.User.User;
 import com.udemy.Teste.Book.User.UserRepository;
 
@@ -23,21 +24,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookController {
 
     @Autowired
+    private CategoryRepository  CategoryRepository;
+
+    @Autowired
     private BookRepository BookRepository;
 
     @Autowired
     private UserRepository UserRepository;
     List<Book> books;
+
+    Integer userconnected=this.getConnectedId();
+
     @GetMapping(value = "/books")
     public ResponseEntity ListBooks(@RequestParam BookStatus status) {
 
 //free books
 if(status==BookStatus.free && status!=null){
     
-    List<Book>books =BookRepository.FindBystatusAndUserIdNotAndDeletedFalse(status, getConnectedId());
+    List<Book>books =BookRepository.FindBystatusAndUserIdNotAndDeletedFalse(status,userconnected);
 } else  { 
 
-  books= BookRepository.FindByUserIdAndDeletedFalse(getConnectedId());}
+  books= BookRepository.FindByUserIdAndDeletedFalse(userconnected);}
 
         return new ResponseEntity<>(books, HttpStatus.OK);
 
@@ -51,7 +58,8 @@ if(status==BookStatus.free && status!=null){
     @PostMapping(value = "/books")
     public ResponseEntity addBook(@Valid @RequestBody Book book) {
 
-       
+  books= BookRepository.FindByUserIdAndDeletedFalse(userconnected);}
+      Optional<User>User= UserRepository.findById(userconnected);
        
     }
 }
