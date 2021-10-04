6c1bea969fe2705c7744e1e17d6ca8fc361d2a31
OUZEREN YASSINE <Yassineouzeren@outlook.fr>
2021-10-04 14:29:58 +0100

    LAst Modification

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO category(label) VALUES("BD");
+INSERT INTO category(label) VALUES("Essai");
+INSERT INTO category(label) VALUES("Cuisine");
+INSERT INTO category(label) VALUES("Bricolage");
+
+
+
+insert into User (firstname ,lastname) VALUES("Myfirstname" ,"Mylastname");
